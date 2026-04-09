5d44375b81b0e1861431b1d1f943dacf86f58e05
Saeedrido <saeed@1791technology.com>
2026-04-09 20:45:06 +0100

    update made

--- src/index.js
+++ src/index.js
@@ -12,7 +12,14 @@ const connectDB = require(path.join(__dirname, '..', 'config', 'database'));
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://team-oscar-frontend-march.onrender.com'],
+  origin: function(origin, callback) {
+    const allowedOrigins = ['http://localhost:5173', 'https://team-oscar-frontend-march.onrender.com'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
